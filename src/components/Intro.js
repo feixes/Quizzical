4c91a5961bd13fc7114507676f5793b14da12a0e
Xavier <xavierfeixesguell@gmail.com>
2021-12-18 01:37:59 +0100

    intro section CSS

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div className='intro'>
+            <h1 className='intro--title'>Quizzical</h1>
+            <p className='intro--desc'>Some description if needed</p>
+            <button className='btn'>Start quiz!</button>
+        </div>
+
+    )
+}
+
+export default Intro
