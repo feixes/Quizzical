2cd1bdb0e2b46dc6b4f59d7110aca893e74853b2
Xavier <xavierfeixesguell@gmail.com>
2021-12-18 13:41:10 +0100

    wired start button to start the quiz

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Intro from './components/Intro'
 import Question from './components/Question'
 
 
 const App = () => {
+
+    const [start, setStart] = useState(false)
+
+    const startQuiz = () => {
+        setStart(true)
+    }
+
     return (
         <main className='container'>
-            {/* <Intro /> */}
+            {!start ?
+                <Intro start={startQuiz} /> :
+                <div>
+                    <Question />
+                    <Question />
+                    <Question />
+                    <Question />
+                    <button class='btn btn-check'>Check asnwers</button>
+                </div>
 
-            <Question />
-            <Question />
-            <Question />
-            <Question />
-            <button class='btn btn-check'>Check asnwers</button>
+
+            }
+
         </main>
 
     )
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Intro = () => {
+const Intro = (props) => {
     return (
         <div className='intro'>
             <h1 className='intro--title'>Quizzical</h1>
             <p className='intro--desc'>Some description if needed</p>
-            <button className='btn btn-intro'>Start quiz!</button>
+            <button onClick={props.start} className='btn btn-intro'>Start quiz!</button>
         </div>
 
     )
